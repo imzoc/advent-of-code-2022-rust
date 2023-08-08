99515ba56ec4fc5a89028a60dd5f0c9dddeb8a34
Zach Keyes <56801478+imzoc@users.noreply.github.com>
2023-08-08 15:34:26 -0700

    completed day 3

--- day3/day3.rs
+++ day3/day3.rs
@@ -1,6 +1,7 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write};
+use std::convert::TryInto;
 
 fn find_misplaced_letters<T>(lines: T) -> Result<Vec<char>, std::io::Error>
 where T: Iterator<Item = Result<String, std::io::Error>>,
@@ -24,6 +25,32 @@ where T: Iterator<Item = Result<String, std::io::Error>>,
     Ok(misplaced_letters)
 }
 
+fn sum_priorities(misplaced_letters: Vec<char>) -> i32 {
+    let mut sum = 0;
+
+    for letter in misplaced_letters {
+        sum += evaluate_priority(&letter)
+    }
+
+    sum
+}
+
+fn evaluate_priority(letter: &char) -> i32 {
+    // Create HashMap of priority values for letters
+    let mut letter_values = HashMap::new();
+    for (index, letter) in ('a'..='z').enumerate() {
+        letter_values.insert(letter, index + 1);
+    }
+    for (index, letter) in ('A'..='Z').enumerate() {
+        letter_values.insert(letter, index + 27);
+    }
+
+    // Return the value of the letter passed to the function as determined by the HashMap
+    match letter_values.get(&letter) {
+        Some(&value) => value.try_into().unwrap(),
+        None => 0
+    }
+}
 // TODO: assign priority values to characters, calculate the priority values of the characters returned by
 // find_misplaced_letters, and then add them all up!
 
@@ -34,8 +61,12 @@ fn main() -> io::Result<()> {
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
 
-    let answer = find_misplaced_letters(reader.lines())?;
+    // Calculate the answer
+    let misplaced_letters = find_misplaced_letters(reader.lines())?;
+    let answer = sum_priorities(misplaced_letters);
+    
     println!("Answer is: {:?}", answer);
-
+    let mut output_file = File::create("answer.txt")?;
+    write!(output_file, "{}", answer)?;
     Ok(())
 }
